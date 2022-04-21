1b0f9d9978c49aa5d5a75876842626f031a504cd
vakhitovalex <vakhitovalex@gmail.com>
2022-04-21 16:30:52 +0300

    refactor: show buttons from models dictionary

--- main.py
+++ main.py
@@ -34,10 +34,8 @@ def cmd_dataset_info(message):
 @bot.message_handler(commands=['models_info'])
 def cmd_models_info(message):
     user_markup = types.ReplyKeyboardMarkup()
-    lr_button = types.KeyboardButton("Линейная регрессия")
-    rf_button = types.KeyboardButton("Случайный лес")
-    mlp_button = types.KeyboardButton("Многослойный перцептрон")
-    user_markup.row(lr_button, rf_button, mlp_button)
+    for button in models_dict:
+        user_markup.row(button)
     bot.send_message(message.chat.id, "Выберите интересующую модель", reply_markup=user_markup)
 
 
